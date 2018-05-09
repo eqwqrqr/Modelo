5a11227bffe2e9adf13aaba9f0dfef1b80be588e
Arlert <punkypep@gmail.com>
2018-05-09 08:43:02 +0200

    Add files via upload

---
+++ Ej 1 Funciones.py
@@ -0,0 +1,13 @@
+import math
+
+def f():
+  math.sqrt(x1+y1) - math.sqrt(x2+y2)
+
+print ("x1") 
+x1= int(input())
+print ("y1") 
+y1= int(input())
+print ("x2") 
+x2= int(input())
+print ("y2") 
+y2= int(input())
